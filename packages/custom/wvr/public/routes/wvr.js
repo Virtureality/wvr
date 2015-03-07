e970b8f506ee6690b4fd48728994576531d5c42d
sharphill <sharphill@163.com>
2015-03-07 18:47:46 +0800

    Replace soon as wvr home by generated home page.

--- packages/custom/wvr/public/routes/wvr.js
+++ packages/custom/wvr/public/routes/wvr.js
@@ -10,12 +10,12 @@ angular.module('mean.wvr').config(['$stateProvider',
 ]);
 
 // Override system home page with wvr home page.
-/*angular.module('mean.wvr', ['mean.system'])
+angular.module('mean.wvr', ['mean.system'])
 .config(['$viewPathProvider', function($viewPathProvider) {
   $viewPathProvider.override('system/views/index.html', 'wvr/views/index.html');
 }]);
-*/
-angular.module('mean.wvr', ['mean.system'])
+
+/*angular.module('mean.wvr', ['mean.system'])
 .config(['$viewPathProvider', function($viewPathProvider) {
   $viewPathProvider.override('system/views/index.html', 'wvr/views/soon.html');
-}]);
+}]);*/
