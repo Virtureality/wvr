a8b21a763b20c3d38c63a8aed73f3e5f2592729c
sharphill <sharphill@163.com>
2015-03-08 10:18:32 +0800

    Put mean system home as wvr home.

--- packages/custom/wvr/public/routes/wvr.js
+++ packages/custom/wvr/public/routes/wvr.js
@@ -10,10 +10,10 @@ angular.module('mean.wvr').config(['$stateProvider',
 ]);
 
 // Override system home page with wvr home page.
-angular.module('mean.wvr', ['mean.system'])
+/*angular.module('mean.wvr', ['mean.system'])
 .config(['$viewPathProvider', function($viewPathProvider) {
   $viewPathProvider.override('system/views/index.html', 'wvr/views/index.html');
-}]);
+}]);*/
 
 /*angular.module('mean.wvr', ['mean.system'])
 .config(['$viewPathProvider', function($viewPathProvider) {
