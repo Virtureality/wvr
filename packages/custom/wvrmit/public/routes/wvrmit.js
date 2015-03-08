0e1b50f6dade82feeeea34bb186e0205816a2fae
sharphill <sharphill@163.com>
2015-03-08 20:43:22 +0800

    Initial MeanSocket-derived wvrmit layout.

--- packages/custom/wvrmit/public/routes/wvrmit.js
+++ packages/custom/wvrmit/public/routes/wvrmit.js
@@ -1,10 +1,45 @@
 'use strict';
 
-angular.module('mean.wvrmit').config(['$stateProvider',
+/*angular.module('mean.wvrmit').config(['$stateProvider',
   function($stateProvider) {
     $stateProvider.state('wvrmit', {
       url: '/wvrmit',
       templateUrl: 'wvrmit/views/index.html'
     });
   }
+]);*/
+
+angular.module('mean.wvrmit')
+.run(function ($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+})
+.config(['$stateProvider',
+  function($stateProvider) {
+    $stateProvider.state('wvrmit', {
+      url: '/wvrmit',
+      templateUrl: 'wvrmit/views/index.html'
+    }).state('wvrmitmit', {
+      url: '/wvrmit/mit/:mname',
+      templateUrl: 'wvrmit/views/mit.html'
+    });
+  }
 ]);
+
+/*angular.module('mean.wvrmit')
+.run(function ($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+})
+.config(['$stateProvider',
+  function($stateProvider) {
+    $stateProvider
+    .state('wvrmit', {
+      url: '/wvrmit',
+      templateUrl: 'wvrmit/views/index.html'
+    }).state('wvrmit.mit', {
+      url: '/mit/:mname',
+      templateUrl: 'wvrmit/views/mit.html'
+    });
+  }
+]);*/
