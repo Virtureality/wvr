749e7663b62f99f81aa0cb7e5b695daa601c0e52
sharphill <sharphill@163.com>
2015-03-26 13:47:59 +0800

    Custom WVR routing.

--- packages/custom/wvr/server/routes/wvr.js
+++ packages/custom/wvr/server/routes/wvr.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var mean = require('meanio');
+
 /* jshint -W098 */
 // The Package is past automatically as first parameter
 module.exports = function(Wvr, app, auth, database) {
 
-  app.get('/wvr/example/anyone', function(req, res, next) {
+/*  app.get('/wvr/example/anyone', function(req, res, next) {
     res.send('Anyone can access this');
   });
 
@@ -23,5 +25,23 @@ module.exports = function(Wvr, app, auth, database) {
       //Rendering a view from the Package server/views
       res.send(html);
     });
+  });*/
+
+  // Home route
+  app.route('/wvr')
+    .get(function(req, res, next) {
+          Wvr.render('home', {
+            package: 'wvr'
+          }, function(err, html) {
+            //Rendering a view from the Package server/views
+            res.send(html);
+          });
+        });
+
+
+  app.get('/wvr/*',function(req,res,next){
+        res.header('workerID' , JSON.stringify(mean.options.workerid) );
+        next(); // http://expressjs.com/guide.html#passing-route control
   });
+
 };
