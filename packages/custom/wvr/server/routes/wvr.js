63ac956ebfd1261505527e2ae9db11ccb4f5d6d5
sharphill <sharphill@163.com>
2015-05-07 15:45:58 +0800

    wvr package Refactor

--- packages/custom/wvr/server/routes/wvr.js
+++ packages/custom/wvr/server/routes/wvr.js
@@ -6,42 +6,16 @@ var mean = require('meanio');
 // The Package is past automatically as first parameter
 module.exports = function(Wvr, app, auth, database) {
 
-/*  app.get('/wvr/example/anyone', function(req, res, next) {
-    res.send('Anyone can access this');
-  });
-
-  app.get('/wvr/example/auth', auth.requiresLogin, function(req, res, next) {
-    res.send('Only authenticated users can access this');
-  });
-
-  app.get('/wvr/example/admin', auth.requiresAdmin, function(req, res, next) {
-    res.send('Only users with Admin role can access this');
-  });
-
-  app.get('/wvr/example/render', function(req, res, next) {
-    Wvr.render('index', {
-      package: 'wvr'
-    }, function(err, html) {
-      //Rendering a view from the Package server/views
-      res.send(html);
-    });
-  });*/
-
   // Home route
-  app.route('/wvr')
+  app.route('/wvr/*')
     .get(function(req, res, next) {
-          Wvr.render('home', {
-            package: 'wvr'
-          }, function(err, html) {
-            //Rendering a view from the Package server/views
-            res.send(html);
-          });
-        });
-
-
-  app.get('/wvr/*',function(req,res,next){
-        res.header('workerID' , JSON.stringify(mean.options.workerid) );
-        next(); // http://expressjs.com/guide.html#passing-route control
-  });
+    	res.header('workerID' , JSON.stringify(mean.options.workerid) );
+    	res.render('home', {
+    		package: 'wvr'
+    	}, function(err, html) {
+    		//Rendering a view from the Package server/views
+    		res.send(html);
+    	});
+    });
 
 };
