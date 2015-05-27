cc070f1e4450e2f0c3d008913ee7565cc922c4d5
sharphill <sharphill@163.com>
2015-05-27 12:13:19 +0800

    ngDragDrop setup for module space with simple samples.

--- packages/custom/wvr/public/app.js
+++ packages/custom/wvr/public/app.js
@@ -32,4 +32,33 @@
       function($scope, $stateParams, Space){
         $scope.space = Space.get({spaceId: $stateParams.spaceId});
       }]);*/
-angular.module('wvr.space', []);
+//angular.module('wvr.space', ['ngDragDrop']);
+
+var spaceApp = angular.module('wvr.space', ['ngDragDrop']);
+
+spaceApp.controller('oneCtrl', function($scope, $timeout) {
+    $scope.list1 = [];
+    $scope.list2 = [];
+    $scope.list3 = [];
+    $scope.list4 = [];
+
+    $scope.list5 = [
+        { 'title': 'Item 1', 'drag': true },
+        { 'title': 'Item 2', 'drag': true },
+        { 'title': 'Item 3', 'drag': true },
+        { 'title': 'Item 4', 'drag': true },
+        { 'title': 'Item 5', 'drag': true },
+        { 'title': 'Item 6', 'drag': true }
+    ];
+
+    // Limit items to be dropped in list1
+    $scope.optionsList1 = {
+        accept: function(dragEl) {
+            if ($scope.list1.length >= 3) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    };
+});
