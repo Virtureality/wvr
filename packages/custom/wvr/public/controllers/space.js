d8680a1399cdddea2605dc87a19d246d0fc635ca
sharphill <sharphill@163.com>
2015-05-28 16:53:50 +0800

    ngDragDrop for space - v1 - Brief.

--- packages/custom/wvr/public/controllers/space.js
+++ packages/custom/wvr/public/controllers/space.js
@@ -13,10 +13,19 @@ angular.module('wvr.space')
     .controller('SpaceDetailController', ['$scope', '$stateParams', 'Space',
       function($scope, $stateParams, Space){
         $scope.space = Space.get({spaceId: $stateParams.spaceId});
-        $scope.designerResources = [
-          { 'title': 'Design Resource 1', 'drag': true },
-          { 'title': 'Design Resource  2', 'drag': true },
-          { 'title': 'Design Resource  3', 'drag': true }
-        ];
-        $scope.designResult = [];
+        $scope.designerResources = {
+          "facilities": [
+              {
+                "name": "Facility - Office",
+                "type": "Facility-Office"
+              }
+          ],
+          "spaces": [
+            {
+              "name": "Space - Generic",
+              "type": "Space-Generic"
+            }
+          ]
+        };
+        $scope.trash = [];
       }]);
