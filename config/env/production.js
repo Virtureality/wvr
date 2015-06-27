1bde64ad6ce39dc9f8869b00692e5be98dc457e1
sharphill <sharphill@163.com>
2015-06-27 20:24:17 +0800

    Customizable MongoDB with default MongoLab.

--- config/env/production.js
+++ config/env/production.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + process.env.MONGODB_URI || 'wvr:wvr11$@ds031751.mongolab.com:31751/wvr',
-  //db: 'mongodb://' + process.env.MONGODB_URI || 'wvr:wvr11$@proximus.modulusmongo.net:27017/Igu9zyzo',
+  db: process.env.MONGODB_URI || 'mongodb://wvr:wvr11$@ds031751.mongolab.com:31751/wvr',
+  //db: process.env.MONGODB_URI || 'mongodb://wvr:wvr11$@proximus.modulusmongo.net:27017/Igu9zyzo',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
