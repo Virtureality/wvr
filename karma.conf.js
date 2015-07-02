9b6d5023d8a06f6eff3400a7ee86844180a9688f
sharphill <sharphill@163.com>
2015-07-02 11:32:58 +0800

    Set Karma urlRoot: as __karma__

--- karma.conf.js
+++ karma.conf.js
@@ -47,6 +47,8 @@ module.exports = function(config) {
     proxies: {
       '/': 'http://localhost:3001/'
     },
+
+    urlRoot: '__karma__',
     
     // enable / disable colors in the output (reporters and logs)
     colors: true,
